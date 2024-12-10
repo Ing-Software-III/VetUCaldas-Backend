6d312ae1af3236c186af73ca29737ad3d8767058
luis <luis.1701820131@ucaldas.edu.co>
2024-12-10 15:35:46 -0500

    adicion variables de entorno

--- core/config.py
+++ core/config.py
@@ -1,7 +1,12 @@
 from pymongo import MongoClient
-
-MONGO_DETAILS = "mongodb+srv://albert1701920351:E8T7mtKYhFLbDMXY@cluster-vetucaldas.34etx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-VetUcaldas"
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
+username = os.getenv("DBUSERNAME")
+password = os.getenv("DBPASSWORD")
+MONGO_DETAILS = "mongodb+srv://"+username+":"+password+"@cluster-vetucaldas.34etx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-VetUcaldas"
+print(MONGO_DETAILS)
+print()
 client = MongoClient(MONGO_DETAILS)
 database = client["vetucaldas"]
 citas_collection = database["citas"]
